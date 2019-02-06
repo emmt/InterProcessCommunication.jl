29b0f0faca1bab8566e9b287a709dd8d83ed0260
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2019-02-06 15:01:26 +0100

    Do not use prettyurls option

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,21 @@
 using Documenter
+
 push!(LOAD_PATH,"../src/")
 using IPC
-DEPLOYDOCS = (get(ENV, "CI", nothing) == "true")
+
+DEPLOYDOCS = true #(get(ENV, "CI", nothing) == "true")
 
 makedocs(
     sitename = "IPC.jl Package",
-    format = Documenter.HTML(prettyurls = DEPLOYDOCS),
+    #format = Documenter.HTML(
+    #    prettyurls = DEPLOYDOCS,
+    #),
+    authors = "Éric Thiébaut and contributors",
     pages = ["index.md", "semaphores.md", "reference.md"]
 )
 
 if DEPLOYDOCS
-    deploydocs(repo = "github.com/emmt/IPC.jl.git")
+    deploydocs(
+        repo = "github.com/emmt/IPC.jl.git",
+    )
 end
